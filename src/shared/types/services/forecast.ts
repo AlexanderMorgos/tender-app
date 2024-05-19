4ad68c8a66846dc5f8bc36eaccebbd09e7f03528
Alexander Morgos <alexander.morgos@intellectsoft.net>
2024-05-20 02:01:12 +0300

    initial commit

--- src/shared/types/services/forecast.ts
+++
@@ -1,5 +0,0 @@
-import { ForecastReadModel } from "@shared/models/forecast/read-model";
-
-export interface IForecastService {
-  fetchForecast: () => Promise<Array<ForecastReadModel>>;
-}
--- src/shared/types/view-models/forecast.ts
+++
@@ -1,6 +0,0 @@
-import { ForecastReadModel } from "@shared/models/forecast/read-model";
-
-export interface IForecastViewModel {
-  forecast: Array<ForecastReadModel>;
-  fetchForecast: () => Promise<void>;
-}
